04d5141007cfa7a6d56ce4bf69075f1898ffef33
Nilotpal Barpujari <nilotpal.barpujari@preventure.live>
2018-09-19 23:52:17 +1000

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# vue-error-catch
+# Vue Error Catch
+
+Trigger / catch custom errors in vue components.
+
+Setup error listeners within Vue component to listen on errors generated anywhere in the component tree. The plugin makes it easy to trigger custom errors and notify interested components. Errors are propagated hierarchically from bottom to top. Every handler in the chain gets an opportunity to handle the error and or let it propagate it to the parent.
+
+## Install
+
+`npm install vue-error-catch --save`
+
+## Usage
+
+```js
+import Vue from "vue";
+import ErrorCatch from "vue-error-catch";
+
+Vue.use(ErrorCatch);
+
+const errorView = {
+  template: "<div>Error view</div>"
+};
+
+const childComponent = {
+  template: `<div><error-view v-if="renderErrorView"/></div>`,
+  components: {
+    errorView
+  },
+  data: () => ({
+    hasError: false
+  }),
+  computed: {
+    renderErrorView() {
+      return this.hasError;
+    }
+  },
+  methods: {
+    $catch(e: any) {
+      if (e.code === 101) {
+        // handle the error
+        this.hasError = true;
+
+        // stop propagation
+        return false;
+      }
+    }
+  }
+};
+
+const parentComponent = {
+  template: `<div><child/></div>`,
+  components: {
+    child: childComponent
+  },
+  methods: {
+    $catch(e: any) {
+      // this will never be triggered because child component
+      // handled the error and returned false.
+    }
+  }
+};
+
+const mainComponent = {
+  template: "<div><parent/></div>",
+  components: {
+    parent: parentComponent
+  },
+  mounted() {
+    // trigger an error on the component tree
+    this.$error({
+      code: 101,
+      message: "A custom error"
+    });
+  }
+};
+```
+
+## API
+
+- `$catch`: Called when a custom error is triggered anywhere in the component tree. Receives the thrown error as the only argument.
+- `$error`: trigger a custom error. The `error` argument is passed to `$catch` hooks on the component tree until it is handled or the last hook is reached.
+
+## Error Propagation
+
+- `$catch` hooks are invoked from bottom to top, i.e. if a parent and child component both declares $catch hooks then the child's hook will be executed before the parent's.
+- A `$catch` hook may return `false` to prevent the error from propagating further.
--- src/plugin.spec.ts
+++ src/plugin.spec.ts
@@ -6,6 +6,21 @@ describe("Vue error plugin", () => {
     const Vue = createLocalVue();
     Vue.use(ErrorHandler);
 
+    const mainComponent = {
+      template: "<div/>"
+    };
+
+    const wrapper = mount(mainComponent, {
+      localVue: Vue
+    });
+
+    expect(wrapper.vm.$error).toBeInstanceOf(Function);
+  });
+
+  it("triggers $catch in all component", () => {
+    const Vue = createLocalVue();
+    Vue.use(ErrorHandler);
+
     const childHandler = jest.fn();
     const grandChildHandler = jest.fn();
 
@@ -42,8 +57,157 @@ describe("Vue error plugin", () => {
       localVue: Vue
     });
 
-    expect(wrapper.vm.$error).toBeInstanceOf(Function);
     expect(childHandler).toHaveBeenCalled();
     expect(grandChildHandler).toHaveBeenCalled();
+  });
+
+  it("triggers handlers bottom to top", () => {
+    const Vue = createLocalVue();
+    Vue.use(ErrorHandler);
+
+    const callSequence = [];
+
+    const grandChildComponent = {
+      template: "<div>grandchild component</div>",
+      methods: {
+        $catch(e: Error) {
+          callSequence.push("GRANDCHILD");
+        }
+      }
+    };
+    const childComponent = {
+      template: "<div><grandchild/></div>",
+      components: {
+        grandchild: grandChildComponent
+      },
+      methods: {
+        $catch(e: Error) {
+          callSequence.push("CHILD");
+        }
+      }
+    };
+    const mainComponent = {
+      template: "<div><child/></div>",
+      components: {
+        child: childComponent
+      },
+      mounted() {
+        this.$error(new Error("error in mounted method"));
+      }
+    };
+
+    mount(mainComponent, {
+      localVue: Vue
+    });
+
+    expect(callSequence).toEqual(["GRANDCHILD", "CHILD"]);
+  });
+
+  it("stops propagation on returning false", () => {
+    const Vue = createLocalVue();
+    Vue.use(ErrorHandler);
+
+    const childOneHandler = jest.fn();
+    const childTwoHandler = jest.fn();
+
+    const childOne = {
+      template: "<div/>",
+      methods: {
+        $catch(e: Error) {
+          childOneHandler(e);
+        }
+      }
+    };
+    const childTwo = {
+      template: "<div/>",
+      methods: {
+        $catch(e: Error) {
+          childTwoHandler(e);
+          return false;
+        }
+      }
+    };
+    const mainComponent = {
+      template: "<div><child-one/><child-two/></div>",
+      components: {
+        childOne,
+        childTwo
+      },
+      mounted() {
+        this.$error(new Error("error in mounted method"));
+      }
+    };
+
+    mount(mainComponent, {
+      localVue: Vue
+    });
+
+    expect(childOneHandler).not.toHaveBeenCalled();
+    expect(childTwoHandler).toHaveBeenCalled();
+  });
+
+  it("error can be any object", () => {
+    const Vue = createLocalVue();
+    Vue.use(ErrorHandler);
+
+    const errorView = {
+      template: "<div>Error view</div>"
+    };
+
+    const childComponent = {
+      template: `<div><error-view v-if="renderErrorView"/></div>`,
+      components: {
+        errorView
+      },
+      data: () => ({
+        hasError: false
+      }),
+      computed: {
+        renderErrorView() {
+          return this.hasError;
+        }
+      },
+      methods: {
+        $catch(e: any) {
+          if (e.code === 101) {
+            this.hasError = true;
+
+            // stop error propagation
+            return false;
+          }
+        }
+      }
+    };
+
+    const parentComponent = {
+      template: `<div><child/></div>`,
+      components: {
+        child: childComponent
+      },
+      methods: {
+        $catch(e: any) {
+          // this will never be triggered
+        }
+      }
+    };
+
+    const mainComponent = {
+      template: "<div><parent/></div>",
+      components: {
+        parent: parentComponent
+      },
+      mounted() {
+        this.$error({
+          code: 101,
+          message: "A custom error"
+        });
+      }
+    };
+
+    const wrapper = mount(mainComponent, { localVue: Vue });
+
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.find("div").text()).toBe("Error view");
+    });
   });
 });
